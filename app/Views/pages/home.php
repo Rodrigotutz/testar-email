6b0cac7d905f2befb38b27593c64b3be3c707d8d
Rodrigo Tutz <rodrigontunestutz@gmail.com>
2023-01-02 16:48:26 -0300

    Criação da view welcome

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,2 +1,22 @@
 <?php $this->layout("components/theme") ?>
-<h1>Página Funcionando</h1>
+
+<div class="welcome">
+    <h1 class="welcome-title">Bem Vindo ao TutzWork</h1>
+    <h3 class="welcome-subtitle">O seu framework para aplicações simples e rápidas</h3>
+
+    <ul class="welcome-links">
+        <li>
+            <a  target="_blank" href="https://github.com/Rodrigotutz/tutzwork">Github</a>
+        </li> 
+        <li>
+            <a href="https://www.facebook.com/people/Tutz-Desenvolvimento/100084962497191/" target="_blank">Facebook</a>
+        </li> 
+        <li>
+            <a href="https://www.instagram.com/rodrigo_tutz/" target="_blank">Instagram</a>
+        </li> 
+    </ul>
+</div>
+
+<footer class="welcome-footer">
+    <h3>&copy; Desenvolvido por <a href="http://rodrigotutz.rf.gd/" target="_blank">Tutz</a> - 2023</h3>
+</footer>
